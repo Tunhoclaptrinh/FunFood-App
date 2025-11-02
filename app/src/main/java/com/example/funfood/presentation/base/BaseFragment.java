232c3d836515e7dc658f412746bdabbc6b185e48
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2025-11-02 16:58:17 +0700

    fix margin bottom

--- app/src/main/java/com/example/funfood/presentation/base/BaseFragment.java
+++ app/src/main/java/com/example/funfood/presentation/base/BaseFragment.java
@@ -29,8 +29,8 @@ public abstract class BaseFragment<VB extends ViewBinding> extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        setupViews();
-        observeData();
+//        setupViews();
+//        observeData();
     }
 
     /**
